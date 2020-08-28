12409ecb94b488589c02474a3336ea2fc242b3f7
lerriuqs <631253076@QQ.COM>
2020-08-28 23:28:38 +0800

    finished part of lec5

---
+++ lecture code and exercises/5. DLLists, Arrays/DList.java
@@ -0,0 +1,15 @@
+/**Say we have a 2 dimensional DList. 
+We want this 2-D DList to be as even as possible. 
+To do this we will try to fill up rows as uniformly as possible- 
+meaning that not row will have a greater size than any other row 
+by more than 1 element. 
+Write a method that will take in a sub DList and add the given element 
+if it fulfills the constraints. If the constraints are not fulfilled, 
+the item will be attempted to be put in the DList below the one 
+you attempted to insert in originally and so forth until the bottom 
+most DList is reached in which case move to the top DList.
+*/
+
+/**
+https://github.com/yifanyangl/Berkeley-CS61B-exercise/blob/c4062c09231ddc6d6e90b65f5186b4bf6d771e39/src/main/java/lec05/TwoDimDLList.java
+*/
---
+++ lecture code and exercises/5. DLLists, Arrays/Deck.java
@@ -0,0 +1,24 @@
+ /** B Level exercise*/
+ public class Deck{
+     public static int[] cards;
+     Deck(){
+         cards = new int[]{1, 3, 4, 10};
+     }
+     public static void main(String[] args) {
+     	Deck dingie = new Deck();
+     	dingie.cards[3] = 3;
+     	/** 1, 3, 4, 3 */
+
+     	Deck pilates = new Deck();
+     	pilates.cards[1] = 2;
+     	/** pilates: 1, 2, 4, 10 */
+     	/** dingie: 1, 2, 4, 10 */
+
+     	int[] newArrWhoDis = {2, 2, 4, 1, 3};
+     	dingie.cards = pilates.cards;
+     	pilates.cards = newArrWhoDis;
+     	newArrWhoDis = null;
+     	/** pilates: 2, 2, 4, 1, 3 */
+     	/** dingie: 2, 2, 4, 1, 3 */     
+     } 
+}
