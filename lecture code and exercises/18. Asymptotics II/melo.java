c4c01bd17732a77143ea6073c3e9389d356650f3
lerriuqs <631253076@QQ.COM>
2020-09-28 22:11:55 +0800

    lec18

---
+++ lecture code and exercises/18. Asymptotics II/melo.java
@@ -0,0 +1,10 @@
+public class melo{
+	public void melo(int N){
+		for(int i = 0; i < N*N; i++){
+			System.out.println("Gelo is fruit pudding");
+		}
+		for(int i = 0; i < N*N*N; i++){
+			System.out.println("Zo Two the Warriors");
+		}
+	}
+}
