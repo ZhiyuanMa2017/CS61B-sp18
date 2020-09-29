77e9dae1b852c7891c966bea56733a24cb64bd0f
lerriuqs <631253076@QQ.COM>
2020-09-29 22:13:46 +0800

    lec18

---
+++ lecture code and exercises/18. Asymptotics II/grigobreath.java
@@ -0,0 +1,11 @@
+public class grigobreath{
+	public void grigobreath(int N){
+		for(int i  = 0; i < N; i++){
+			System.out.println("Gul-great")
+		}
+		grigobreath(N * 1/2);
+		grigobreath(N * 1/4);
+		grigobreath(N * 1/4);
+	}
+
+}
