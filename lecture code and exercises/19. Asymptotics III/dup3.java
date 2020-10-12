26bceac2030a41c5a17143797b520be9614cdff3
lerriuqs <631253076@QQ.COM>
2020-10-12 22:19:32 +0800

    lec19

---
+++ lecture code and exercises/19. Asymptotics III/dup3.java
@@ -0,0 +1,14 @@
+public class dup3{
+	public boolean dup3(int[] a) {
+	int N = a.length;
+	for (int i = 0; i < N; i += 1) {
+    	for (int j = 0; j < N; j += 1) {
+            if (a[i] == a[j]) {
+                return true;
+            }
+    	}
+	}
+	return false;
+}
+
+}
