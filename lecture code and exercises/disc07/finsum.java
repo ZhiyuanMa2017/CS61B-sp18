86523423c9eae2beeb4d407ca701b0c63defc601
lerriuqs <631253076@QQ.COM>
2020-10-13 23:30:36 +0800

    disc07

---
+++ lecture code and exercises/disc07/finsum.java
@@ -0,0 +1,16 @@
+public class finsum{
+	public static boolean findSumFaster(int[] A, int x){
+		int left = 0;
+		int right = A.length - 1;
+		while (left <= right) {
+			if (A[left] + A[right] == x) {
+				return true;
+			} else if (A[left] + A[right] < x) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		return false;
+	}
+}
