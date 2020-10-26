2711360ffa044b8d5d116d7c6079ad3d1f001aea
lerriuqs <631253076@QQ.COM>
2020-10-26 23:21:12 +0800

    lec22

--- lecture code and exercises/22. Balanced BSTs/InnerNode2_4.java
+++ lecture code and exercises/22. Balanced BSTs/InnerNode2_4.java
@@ -1,2 +1,13 @@
-public class InnerNode2_4 {
+class InnerNode2_4 extends Node2_4 {
+    @Override
+    boolean contains(String key) {
+        for (int k = 0; k < size()-1; k += 1 ) {
+            if (key(k).equals(key) ) {
+                return true;
+            } else if (key(k).compareTo(key) > 0 ) {
+                return kid(k ).contains(key);
+            }
+        }
+        return kid(size() - 1).contains(key);
+    }
 }
