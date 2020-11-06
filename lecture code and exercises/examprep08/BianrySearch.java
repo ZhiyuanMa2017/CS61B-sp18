1e7ebd6b82108abc3e84178d23eee03a5aadf805
lerriuqs <631253076@QQ.COM>
2020-11-06 22:43:04 +0800

    finished examprep08

---
+++ lecture code and exercises/examprep08/BianrySearch.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class BianrySearch {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = nums[mid];
+            if (midVal < target)
+                low = mid + 1;
+            else if (midVal > target)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -(low + 1);
+    }
+}
+
